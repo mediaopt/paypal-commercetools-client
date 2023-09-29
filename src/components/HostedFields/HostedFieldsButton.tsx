b8f06f9dc92dddd115d321e7de75eed951b0af45
Jonathan Yeboah <jonathan.yeboah@mediaopt.de>
2023-09-29 15:42:31 +0200

    #34807 removed console logging

--- src/components/HostedFields/HostedFieldsButton.tsx
+++ src/components/HostedFields/HostedFieldsButton.tsx
@@ -1,25 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { usePayment } from "../../app/usePayment";
 import { HostedFieldsMask } from "./HostedFieldsMask";
-import { useHandleCreatePayment } from "../../app/useHandleCreatePayment";
 import { useHandleGetClientToken } from "../../app/useHandleGetClientToken";
-import { ReactPayPalScriptOptions } from "@paypal/react-paypal-js";
 import { HostedFieldsProps } from "../../types";
 
 export const HostedFieldsButton: React.FC<HostedFieldsProps> = ({
   options,
 }) => {
-  // const { paymentInfo } = usePayment();
   useHandleGetClientToken(false);
   const { clientToken } = usePayment();
 
-  useEffect(() => {
-    console.log("change");
-    console.log(!!clientToken);
-  }, [clientToken]);
-
-  // useHandleCreatePayment();
-  // return paymentInfo.id ? <HostedFieldsMask /> : <></>;
   return clientToken ? <HostedFieldsMask options={options} /> : <></>;
 };
