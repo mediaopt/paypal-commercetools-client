8664695fe417539849afc22a4b3c588727b0daeb
Jonathan Yeboah <jonathan.yeboah@mediaopt.de>
2023-10-02 09:52:32 +0200

    #34807 Code Review use component for spans

---
+++ src/components/HostedFields/HostedFieldsInvalid.tsx
@@ -0,0 +1,9 @@
+const HostedFieldsInvalid = () => {
+  const INVALID_COLOR = {
+    color: "#dc3545",
+  };
+
+  return <span style={INVALID_COLOR}>*</span>;
+};
+
+export default HostedFieldsInvalid;
