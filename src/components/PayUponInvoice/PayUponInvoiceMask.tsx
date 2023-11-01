769ce1c12a3da13512c809e7f8880a5ca022ed87
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-11-01 10:53:36 +0100

    34766 - made phone input controlled and added parser to identify country code

--- src/components/PayUponInvoice/PayUponInvoiceMask.tsx
+++ src/components/PayUponInvoice/PayUponInvoiceMask.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import parsePhoneNumber from "libphonenumber-js";
 import { useHandleGetClientToken } from "../../app/useHandleGetClientToken";
 import { usePayment } from "../../app/usePayment";
 import { useNotifications } from "../../app/useNotifications";
@@ -6,38 +7,51 @@ import { InvoiceLegalNote } from "./InvoiceLegalNote";
 import { PayUponInvoiceButtonProps } from "../../types";
 import { STYLED_PAYMENT_BUTTON, STYLED_PAYMENT_FIELDS } from "../../styles";
 
+const parsePhone = (phone: string) => {
+  const parsedPhone = parsePhoneNumber(phone.replaceAll(" ", ""));
+  return parsedPhone
+    ? `+${parsedPhone?.countryCallingCode ?? ""} ${
+        parsedPhone?.nationalNumber ?? ""
+      }`
+    : phone;
+};
+
 export const PayUponInvoiceMask: FC<PayUponInvoiceButtonProps> = ({
   fraudnetSessionId,
   invoiceBenefitsMessage,
   purchaseCallback,
 }) => {
   useHandleGetClientToken(false);
-  const { handleCreateOrder } = usePayment();
-
+  const { handleCreateInvoice } = usePayment();
   const { notify } = useNotifications();
-
-  const paypalInvoiceRequest = async () => {
-    const additionalInvoiceHeader = {
-      "PAYPAL-CLIENT-METADATA-ID": fraudnetSessionId,
-    };
-    //const res = await handleCreateOrder();
-    const res = true;
-    return res;
-  };
+  const [phone, setPhone] = useState("+49 ");
+  const notifyWrongPhone = () =>
+    notify(
+      "Warning",
+      "Could not identify country code or national phone number, please check the data.",
+    );
 
   return (
     <form
       className="my-4"
       onSubmit={async (event) => {
         event.preventDefault();
-        const birthDate = (
-          document.getElementById("birthDate") as HTMLInputElement
-        ).value;
-        const phone = (document.getElementById("phone") as HTMLInputElement)
-          .value;
-        const orderStatus = await paypalInvoiceRequest();
-        if (orderStatus && purchaseCallback) purchaseCallback(orderStatus);
-        else notify("Warning", "Something went wrong");
+        const formData = event.target as HTMLFormElement;
+        const birthDate = formData["birthDate"].value;
+        const actualPhone = parsePhoneNumber(phone);
+        if (actualPhone) {
+          const { countryCallingCode, nationalNumber } = actualPhone;
+          if (countryCallingCode && nationalNumber) {
+            const orderStatus = await handleCreateInvoice({
+              fraudnetSessionId,
+              nationalNumber,
+              countryCallingCode,
+              birthDate,
+            });
+            if (orderStatus && purchaseCallback) purchaseCallback(orderStatus);
+            else notify("Warning", "Something went wrong");
+          } else notifyWrongPhone();
+        } else notifyWrongPhone();
       }}
     >
       {invoiceBenefitsMessage && (
@@ -57,8 +71,10 @@ export const PayUponInvoiceMask: FC<PayUponInvoiceButtonProps> = ({
         type="tel"
         name="phone"
         id="phone"
-        //pattern="^\+(?:[0-9]●?){6,14}[0-9]$"
-        placeholder="+49 123 123 1234"
+        pattern="^\+[0-9]{1,4} [0-9]{0,13}$$"
+        placeholder="+49 1231231234"
+        value={phone}
+        onChange={({ target }) => setPhone(parsePhone(target.value))}
         className={STYLED_PAYMENT_FIELDS}
         autoComplete="tel"
         required
