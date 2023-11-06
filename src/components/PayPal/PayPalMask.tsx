a02a5bafade1d0a6cf18a65447f0ee3b35e2dc45
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2023-11-06 11:47:44 +0100

    apply PayPal button styles

--- src/components/PayPal/PayPalMask.tsx
+++ src/components/PayPal/PayPalMask.tsx
@@ -3,16 +3,30 @@ import { PayPalButtons, PayPalMessages } from "@paypal/react-paypal-js";
 import { CustomPayPalButtonsComponentProps } from "../../types";
 
 import { usePayment } from "../../app/usePayment";
+import { useSettings } from "../../app/useSettings";
 
 export const PayPalMask: React.FC<CustomPayPalButtonsComponentProps> = (
   props
 ) => {
   const { handleCreateOrder, handleOnApprove } = usePayment();
+  const { settings } = useSettings();
   const { paypalMessages, ...restprops } = props;
+
+  if (!settings) {
+    return <></>;
+  }
+
+  const style = {
+    color: settings.paypalButtonConfig.buttonColor,
+    label: settings.paypalButtonConfig.buttonLabel,
+    tagline: settings.buttonTagline,
+    shape: settings.buttonShape,
+  };
   return (
     <>
       <PayPalButtons
         {...restprops}
+        style={style}
         createOrder={handleCreateOrder}
         onApprove={handleOnApprove}
       />
