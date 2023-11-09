0d8865d69f538226bde2372d3e123e26e13f2d38
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-11-09 20:03:09 +0100

    34766 - simplified legal note using predefined translation

--- src/components/PayUponInvoice/InvoiceLegalNote.tsx
+++ src/components/PayUponInvoice/InvoiceLegalNote.tsx
@@ -1,30 +1,25 @@
+import { Trans } from "react-i18next";
+
 const STYLED_LINK = "text-blue-500 cursor-pointer";
 
 export const InvoiceLegalNote = (
   <div className="w-full text-sm te">
-    By clicking on the button, you agree to the{" "}
-    <a
-      className={STYLED_LINK}
-      href="https://www.ratepay.com/legal-payment-terms"
-    >
-      terms of payment
-    </a>{" "}
-    and{" "}
-    <a
-      className={STYLED_LINK}
-      href="https://www.ratepay.com/legal-payment-dataprivacy"
-    >
-      performance of a risk check
-    </a>{" "}
-    from the payment partner, Ratepay. You also agree to PayPal’s{" "}
-    <a
-      className={STYLED_LINK}
-      href="https://www.paypal.com/us/webapps/mpp/ua/privacy-full"
-    >
-      privacy statement
-    </a>
-    . If your request to purchase upon invoice is accepted, the purchase price
-    claim will be assigned to Ratepay, and you may only pay Ratepay, not the
-    merchant.
+    <Trans
+      i18nKey="pUILegalNote"
+      components={[
+        <a
+          className={STYLED_LINK}
+          href="https://www.ratepay.com/legal-payment-terms"
+        />,
+        <a
+          className={STYLED_LINK}
+          href="https://www.ratepay.com/legal-payment-dataprivacy"
+        />,
+        <a
+          className={STYLED_LINK}
+          href="https://www.paypal.com/us/webapps/mpp/ua/privacy-full"
+        />,
+      ]}
+    />
   </div>
 );
