4f81d281feb359e939be7b6f31b9c94295093dc0
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-11-10 13:11:04 +0100

    34766 - moved text styles to styles

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -5,3 +5,5 @@ export const STYLED_PAYMENT_BUTTON =
   "float-right text-center whitespace-nowrap inline-block font-normal align-middle select-none cursor-pointer text-white text-base rounded py-1.5 px-3 bg-sky-500 border-sky-500";
 
 export const STYLED_LINK = "text-blue-500 cursor-pointer";
+export const ERROR_TEXT_STYLE = "text-rose-600";
+export const SUCCESS_TEXT_STYLE = "text-green-700";
