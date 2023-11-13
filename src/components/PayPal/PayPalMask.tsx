1b1798510a1edb06bd20d2ec03f08a69e574f43c
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2023-11-13 09:28:53 +0100

    added onError callback for smart buttons

--- src/components/PayPal/PayPalMask.tsx
+++ src/components/PayPal/PayPalMask.tsx
@@ -3,18 +3,30 @@ import { PayPalButtons, PayPalMessages } from "@paypal/react-paypal-js";
 import { CustomPayPalButtonsComponentProps } from "../../types";
 
 import { usePayment } from "../../app/usePayment";
+import { useLoader } from "../../app/useLoader";
+import { useNotifications } from "../../app/useNotifications";
 
 export const PayPalMask: React.FC<CustomPayPalButtonsComponentProps> = (
   props
 ) => {
   const { handleCreateOrder, handleOnApprove } = usePayment();
+  const { isLoading } = useLoader();
+  const { notify } = useNotifications();
   const { paypalMessages, ...restprops } = props;
+
+  const errorFunc = (err: Record<string, unknown>) => {
+    isLoading(false);
+    notify("Error", "an error occurred");
+    console.error(err);
+  };
+
   return (
     <>
       <PayPalButtons
         {...restprops}
         createOrder={handleCreateOrder}
         onApprove={handleOnApprove}
+        onError={errorFunc}
       />
       {paypalMessages && <PayPalMessages {...paypalMessages} />}
     </>
