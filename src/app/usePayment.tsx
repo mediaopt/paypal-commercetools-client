005ae81cc02c2fb846431a537597f1d7b6c1044e
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-11-17 14:30:22 +0100

    34766 - use updated payment version for ger order call.

--- src/app/usePayment.tsx
+++ src/app/usePayment.tsx
@@ -195,7 +195,7 @@ export const PaymentProvider: FC<
         requestHeader,
         createOrderUrl,
         paymentInfo.id,
-        paymentInfo.version,
+        latestPaymentVersion,
         {
           ...relevantOrderData,
         },
