4064b37ff79d1e1100ee4e0441b234bceeed4180
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2023-12-08 10:50:45 +0100

    export CardFields

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export { PayPalMessages } from "./src/components/PayPalMessages";
 export { HostedFields } from "./src/components/HostedFields";
 export { PaymentTokens } from "./src/components/PaymentTokens";
 export { PayUponInvoice } from "./src/components/PayUponInvoice";
+export { CardFields } from "./src/components/CardFields";
