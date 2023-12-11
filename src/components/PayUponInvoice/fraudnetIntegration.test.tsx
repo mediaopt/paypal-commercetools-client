472384cbd396b252df9e5e654788c4e1a55e7415
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-12-11 14:23:01 +0100

    35214 - added async test for fraudnet loading

--- src/components/PayUponInvoice/fraudnetIntegration.test.tsx
+++ src/components/PayUponInvoice/fraudnetIntegration.test.tsx
@@ -1,9 +1,16 @@
 import { embeddFraudNet } from "./fraudNetIntegration";
-import { screen } from "@testing-library/react";
+import { cleanup, screen } from "@testing-library/react";
+import loadScript from "../../app/loadScript";
 
 const dummyMerchantId = "123";
 const dummyPage = "home-page";
 const dummySetId = (id: string | undefined) => {};
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+  cleanup();
+});
 
 test("Fraudnet script integration is called", () => {
   const result = embeddFraudNet(dummyMerchantId, dummyPage, dummySetId);
@@ -14,4 +21,11 @@ test("Nosript is integrated immediately", async () => {
   await embeddFraudNet(dummyMerchantId, dummyPage, dummySetId);
   const noscriptImage = screen.getAllByRole("img");
   expect(noscriptImage.length).toEqual(1);
+});
+
+jest.mock("../../app/loadScript");
+test("Load script is called", async () => {
+  (loadScript as jest.Mock).mockReturnValue(Promise.resolve());
+  await embeddFraudNet(dummyMerchantId, dummyPage, dummySetId);
+  expect(loadScript).toBeCalled();
 });
