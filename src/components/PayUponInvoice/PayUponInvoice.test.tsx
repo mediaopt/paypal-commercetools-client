8cb20f4fe5b8d97585daef3daa31d495aaceddd0
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-12-12 10:24:49 +0100

    35214 - test for change locale for invoice

--- src/components/PayUponInvoice/PayUponInvoice.test.tsx
+++ src/components/PayUponInvoice/PayUponInvoice.test.tsx
@@ -4,6 +4,7 @@ import { render } from "@testing-library/react";
 import { PayUponInvoiceProps } from "../../types";
 
 import { PayUponInvoice } from "./index";
+import { ReactNode } from "react";
 
 const fraudNetUrl = "https://c.paypal.com/da/r/fb.js";
 
@@ -61,6 +62,7 @@ const paypalInvoiceParams: PayUponInvoiceProps = {
   maxPayableAmount: 2500, //euro
   customLocale: "de",
 };
+
 const requestHeader = {
   "Frontastic-Session": "",
   "Commercetools-Frontend-Extension-Version": "",
@@ -73,10 +75,48 @@ const PayUponInvoiceJson = {
   ...paypalInvoiceParams,
 };
 
+const mockChange = jest.fn();
+jest.mock("react-i18next", () => ({
+  Trans: ({ i18nKey }: { i18nKey: string }) => i18nKey,
+  useTranslation: () => {
+    return {
+      t: (str: string) => str,
+      i18n: {
+        changeLanguage: mockChange(),
+      },
+    };
+  },
+  initReactI18next: {
+    type: "3rdParty",
+    init: jest.fn(),
+  },
+}));
+
+jest.mock("../RenderTemplate", () => ({
+  RenderTemplate: ({ children }: { children: ReactNode }) => (
+    <div>{children}</div>
+  ),
+}));
+
 test("Fraud net script is loaded on PUI render", () => {
   render(<PayUponInvoice {...PayUponInvoiceJson} />);
   const fraudnetScript = document.querySelectorAll(
     `script[src="${fraudNetUrl}"]`,
   );
   expect(fraudnetScript.length).toEqual(1);
+});
+
+const customPaypalInvoiceParams: PayUponInvoiceProps = {
+  merchantId: "",
+  pageId: "checkout-page",
+  minPayableAmount: 5, //euro
+  maxPayableAmount: 2500, //euro
+  customLocale: "ua",
+};
+
+test("If custom locale is set, locale change is called", async () => {
+  render(
+    <PayUponInvoice {...PayUponInvoiceJson} {...customPaypalInvoiceParams} />,
+  );
+  expect(mockChange).toHaveBeenCalled();
 });
