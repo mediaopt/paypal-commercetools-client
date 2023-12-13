9253dd62803ddf82931f7bd53ff8fe4425349649
liudmyla <liudmyla.masliuk@mediaopt.de>
2023-12-13 09:41:14 +0100

    35214 - cleaned the code

--- src/components/HostedFields/HostedFields.test.tsx
+++ src/components/HostedFields/HostedFields.test.tsx
@@ -19,9 +19,6 @@ jest.mock("react-i18next", () => ({
   useTranslation: () => {
     return {
       t: (str: string) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-      },
     };
   },
 }));
--- src/components/PayUponInvoice/PayUponInvoice.test.tsx
+++ src/components/PayUponInvoice/PayUponInvoice.test.tsx
@@ -5,74 +5,22 @@ import { PayUponInvoiceProps } from "../../types";
 
 import { PayUponInvoice } from "./index";
 import { ReactNode } from "react";
+import {
+  testOptions,
+  testParams,
+  testPaypalInvoiceParams,
+  testRequestHeader,
+} from "../../constants";
 
 const fraudNetUrl = "https://c.paypal.com/da/r/fb.js";
 
-const cartInformation = {
-  account: {
-    email: "payment_source_info_cannot_be_verified@example.com",
-  },
-  billing: {
-    firstName: "John",
-    lastName: "Smith",
-    streetName: "Hochstraße",
-    streetNumber: "37",
-    city: "Berlin",
-    country: "DE",
-    postalCode: "12045",
-  },
-  shipping: {
-    firstName: "John",
-    lastName: "Smith",
-    streetName: "Hochstraße",
-    streetNumber: "37",
-    city: "Berlin",
-    country: "DE",
-    postalCode: "12045",
-  },
-};
-
-const commonParams = {
-  getSettingsUrl: " ",
-  getClientTokenUrl: " ",
-  createPaymentUrl: " ",
-  purchaseCallback: (result: any, options: any) => {
-    console.log("Do something", result, options);
-  },
-};
-
-const params = {
-  ...commonParams,
-  createOrderUrl: "",
-  authorizeOrderUrl: "",
-  onApproveUrl: "",
-  shippingMethodId: "",
-  cartInformation: cartInformation,
-};
-
-const options = {
-  clientId: " ",
-  currency: " ",
-};
-
-const paypalInvoiceParams: PayUponInvoiceProps = {
-  merchantId: "",
-  pageId: "checkout-page",
-  minPayableAmount: 5, //euro
-  maxPayableAmount: 2500, //euro
-  customLocale: "de",
-};
-
-const requestHeader = {
-  "Frontastic-Session": "",
-  "Commercetools-Frontend-Extension-Version": "",
-};
-
 const PayUponInvoiceJson = {
-  options,
-  requestHeader,
-  ...params,
-  ...paypalInvoiceParams,
+  options: {
+    ...testOptions,
+  },
+  requestHeader: { ...testRequestHeader },
+  ...testParams,
+  ...testPaypalInvoiceParams,
 };
 
 const mockChange = jest.fn();
