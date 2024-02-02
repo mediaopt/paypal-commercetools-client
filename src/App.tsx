f5997d49a0fd5652702fd3f4766f6e256698a185
liudmyla <liudmyla.masliuk@mediaopt.de>
2024-02-02 14:32:37 +0100

    35619 - updated paypal client id

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,9 @@ import { CardFields } from "./components/CardFields";
 import { PayUponInvoice } from "./components/PayUponInvoice";
 import { PayUponInvoiceProps } from "./types";
 
-const CC_FRONTEND_EXTENSION_VERSION: string = "devjonathanyeboah";
+const CC_FRONTEND_EXTENSION_VERSION: string = "devliudmylamasliuk";
 const FRONTASTIC_SESSION: string =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjYXJ0SWQiOiI5ZjAyZjk1Yi1kNzJkLTRlMDYtOTAyYy03Yzg3MDA5MmIwNmMifQ.hmg1giJkzKlLhMCAhav_N32uDqrfFVlBmyGGwslBTMc";
-
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ3aXNobGlzdElkIjoiZTlhMDlmYjgtOTI5Ny00MTJhLTg2MDMtZTc1NGFiN2VkZmI5IiwiYWNjb3VudCI6eyJhY2NvdW50SWQiOiI3ODQ4MDdkNi00MGQ3LTQ4YzEtYWM3OC0xMzIxNjdhY2MwMTkiLCJlbWFpbCI6ImphbmUuZG9lQGV4YW1wbGUuY29tIiwiZmlyc3ROYW1lIjoiSmFuZSIsImxhc3ROYW1lIjoiRG9lIiwiYmlydGhkYXkiOiIxOTc0LTA5LTIwVDAwOjAwOjAwLjAwMFoiLCJjb25maXJtZWQiOnRydWUsImFkZHJlc3NlcyI6W3siYWRkcmVzc0lkIjoiYjZtMjlqVDQiLCJmaXJzdE5hbWUiOiJKYW5lIiwibGFzdE5hbWUiOiJEb2UiLCJzdHJlZXROYW1lIjoiRmlyc3QgU3RyZWV0Iiwic3RyZWV0TnVtYmVyIjoiMTIiLCJwb3N0YWxDb2RlIjoiMTIzNDUiLCJjaXR5IjoiRXhhbXBsZSBDaXR5IiwiY291bnRyeSI6Ik5MIiwicGhvbmUiOiIrMzEyMzQ1Njc4IiwiaXNEZWZhdWx0QmlsbGluZ0FkZHJlc3MiOmZhbHNlLCJpc0RlZmF1bHRTaGlwcGluZ0FkZHJlc3MiOmZhbHNlfSx7ImFkZHJlc3NJZCI6ImNua3UyWXB6IiwiZmlyc3ROYW1lIjoiSmFuZSIsImxhc3ROYW1lIjoiRG9lIiwic3RyZWV0TmFtZSI6IlRoaXJkIFN0cmVldCIsInN0cmVldE51bWJlciI6IjM0IiwicG9zdGFsQ29kZSI6IjEyMzQ1IiwiY2l0eSI6IkV4YW1wbGUgQ2l0eSIsImNvdW50cnkiOiJOTCIsInBob25lIjoiKzMxMTIzNDU2NzgiLCJpc0RlZmF1bHRCaWxsaW5nQWRkcmVzcyI6ZmFsc2UsImlzRGVmYXVsdFNoaXBwaW5nQWRkcmVzcyI6ZmFsc2V9XX0sImNhcnRJZCI6IjA5YjA5MzQ4LTJiZWYtNDQ0Mi1iZDYzLTAwMDMzZTczMzE5NSJ9.1CTnG3lT-mwKyvFuA_2WKbaJn2TGcyox8x-y0u6LLwE";
 function App() {
   const [choosenPaymentMethod, setChoosenPaymentMethod] = useState("");
 
@@ -87,7 +86,7 @@ function App() {
 
   const options = {
     clientId:
-      "AQlyw_Usbq3XVXnbs2JfrtmDAzJ2ECVzs4WM7Nm9QkoOWb8_s_C6-bkgs0o4ggzCYp_RhJO5OLS_sEi9",
+      "ASIX4GwxjaJ8t603IAUySrbFPTDijGxtNigDRxbuO4E4HVsUzpYYzfVq99MhIZ6dS0AAKjPpeHNj5tyS",
     currency: "EUR",
   };
 
