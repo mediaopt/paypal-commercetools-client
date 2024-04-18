4d78d3c6e1ca9321a442bd05ea50b6e67c7e06f7
Majid Abbasi <majid.abbasi@mediaopt.de>
2024-04-18 16:28:59 +0200

    #36076 applepay vaulting

--- src/components/ApplePay/ApplePayMask.tsx
+++ src/components/ApplePay/ApplePayMask.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
+import { useSettings } from "../../app/useSettings";
 import loadScript from "../../app/loadScript";
 import { usePayment } from "../../app/usePayment";
 import { ERROR_TEXT_STYLE } from "../../styles";
@@ -20,15 +21,18 @@ type ApplePayMaskComponentProps = ApplePayProps &
 export const ApplePayMask: React.FC<ApplePayMaskComponentProps> = (props) => {
   //const [logs, setLogs] = useState<string>();
   //const [paymentId, setPaymentId] = useState<string>();
-
   const [error, setError] = useState<string>();
   const [isEligible, setIsEligible] = useState<boolean>(false);
   const [payConfig, setPayConfig] = useState<ApplepayConfig>();
   const [pay, setPay] = useState<Applepay>();
 
+  const { settings } = useSettings();
   const { paymentInfo, handleCreateOrder, handleOnApprove } = usePayment();
 
-  const { applePayDisplayName } = props;
+  const save = useRef<HTMLInputElement>(null);
+
+  const { applePayDisplayName, enableVaulting } = props;
+  const storeInVaultOnSuccess = settings?.storeInVaultOnSuccess;
 
   useEffect(() => {
     loadScript("https://applepay.cdn-apple.com/jsapi/v1/apple-pay-sdk.js").then(
@@ -114,7 +118,10 @@ export const ApplePayMask: React.FC<ApplePayMaskComponentProps> = (props) => {
       //setPaymentId("payment id: " + paymentInfo.id);
 
       try {
-        const orderId = await handleCreateOrder({ paymentSource: "paypal" });
+        const orderId = await handleCreateOrder({
+          paymentSource: "paypal",
+          storeInVault: save.current?.checked,
+        });
         //setLogs("orderId: " + orderId);
 
         const confirmResult = await pay.confirmOrder({
@@ -142,28 +149,42 @@ export const ApplePayMask: React.FC<ApplePayMaskComponentProps> = (props) => {
     <>
       <div id="applepay-container">
         {isEligible ? (
-          <button
-            onClick={onApplePayButtonClicked}
-            type="button"
-            className="w-full justify-center text-white bg-primary-900 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 mr-2 mb-2"
-          >
-            Check out with Apple Pay
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="apple"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
+          <>
+            <button
+              onClick={onApplePayButtonClicked}
+              type="button"
+              className="w-full justify-center text-white bg-primary-900 focus:ring-4 focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 mr-2 mb-2"
             >
-              <path
-                fill="currentColor"
-                d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-              ></path>
-            </svg>
-          </button>
+              Check out with Apple Pay
+              <svg
+                className="ml-2 -mr-1 w-5 h-5"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="apple"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 384 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+                ></path>
+              </svg>
+            </button>
+            {(enableVaulting || storeInVaultOnSuccess) && (
+              <label>
+                <input
+                  type="checkbox"
+                  id="save"
+                  name="save"
+                  ref={save}
+                  className="mr-1"
+                />
+                Save for future purchases
+              </label>
+            )}
+          </>
         ) : (
           <div className={ERROR_TEXT_STYLE}>{error}</div>
         )}
