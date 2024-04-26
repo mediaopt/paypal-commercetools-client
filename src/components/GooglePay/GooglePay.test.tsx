f3652822bdaa3f1b4adda062bae6b882cc04d9be
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2024-04-26 11:35:48 +0200

    removed tests

--- src/components/GooglePay/GooglePay.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { GooglePay } from "./GooglePay";
-
-import { GooglePayTestParams } from "./constants";
-
-test("GooglePay is shown", () => {
-  render(<GooglePay {...GooglePayTestParams} />);
-  expect(screen).toBeDefined();
-});
